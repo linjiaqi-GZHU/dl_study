b7dd65de8cd89af2f23be4ca6e8a5365396889a2
*lixia332* <*3032118325@qq.com*>
2021-02-01 22:12:56 +0800

    change

---
+++ data_demo.py
@@ -0,0 +1,14 @@
+#shape
+#shape  lixia
+
+
+
+#list  (batch,32,32,3)   (batch,1) ---》img  lixia
+#y_training
+
+#(batch,32,32,3)        true(batch,1)   predict(batch,1)  jiaqi
+
+#num -->>str jiaqi
+
+
+
